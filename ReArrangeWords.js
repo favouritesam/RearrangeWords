4aa69c74ca24fc8b4b387a8c27f79aa6c8db760b
favouritesam <favsamlol>
2023-05-11 13:40:56 +0100

    first commit

---
+++ ReArrangeWords.js
@@ -0,0 +1,19 @@
+let words = "someiclon";
+let array = [0,3,2,4,5,1,6,7,8];
+let arr = "";
+for (let i = 0; i <array.length ; i++) {
+    arr += words[array[i]];
+
+}
+console.log(arr)
+
+let word = "codeleet";
+let arrays = [4,5,6,7,0,1,2,3];
+let empty = "";
+for (let i = 0; i <arrays.length ; i++) {
+    empty += word[arrays[i]];
+
+}
+console.log(empty)
+
+
